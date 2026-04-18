9eb589948f12c0d7fc5bf75a2545c11b6120c714
HI-DEV <deathuntil5@gmail.com>
2026-04-18 17:35:09 +0100

    fix security bug, and update all packages

--- src/rtk/store/store.js
+++ src/rtk/store/store.js
@@ -1,18 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "../cart/cartSlice";
-import logger from "redux-logger";
-import productSlice from '../products/productSlice'
-import wishListSlice from '../wishList/wishListSlice'
+import productSlice from "../products/productSlice";
+import wishListSlice from "../wishList/wishListSlice";
 
 const store = configureStore({
-    reducer: {
-        cart: cartSlice,
-        products: productSlice,
-        wishList: wishListSlice
-    },
-    middleware: (GetDefaultMiddleware) => GetDefaultMiddleware().concat(logger)
-})
+  reducer: {
+    cart: cartSlice,
+    products: productSlice,
+    wishList: wishListSlice,
+  },
+});
 
-export default store
-
-
+export default store;
