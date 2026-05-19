b486aaa5c502d71a23817faf23bd985724a876af
HI-DEV <deathuntil5@gmail.com>
2026-05-19 22:15:32 +0100

    fix: bug

--- src/utils/Layout.jsx
+++ src/utils/Layout.jsx
@@ -1,13 +1,11 @@
 import Header from "../Components/Header";
 import Routes from "../utils/Routing";
-// import Footer from "../Components/Footer";
 
 const Layout = () => {
   return (
     <>
       <Header />
       <Routes />
-      {/* <Footer /> */}
     </>
   );
 };
